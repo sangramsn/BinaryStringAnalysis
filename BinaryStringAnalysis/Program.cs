caec4d18bf99e8d563d49a93041c6e2044bcf2c2
sangramsn <sangramsn191@gmail.com>
2024-09-30 20:05:59 +0530

    initial commit
    initial commit

---
+++ BinaryStringAnalysis/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class BinaryStringEvaluator
+{
+    public static bool IsGoodBinaryString(string binaryString)
+    {
+        int count0 = 0;
+        int count1 = 0;
+
+        foreach (char c in binaryString)
+        {
+            if (c == '0')
+            {
+                count0++;
+            }
+            else if (c == '1')
+            {
+                count1++;
+            }
+            else
+            {
+                throw new ArgumentException("Input string only need to contain '0' and '1'.");
+            }
+
+            
+            if (count1 < count0)
+            {
+                return false; 
+            }
+        }
+
+       
+        return count0 == count1;
+    }
+
+    public static void Main(string[] args)
+    {
+        
+        string[] Example = { "1100", "1010", "1001", "111000", "0101", "0011", "110011", "11100000" };
+
+        foreach (var test in Example)
+        {
+            Console.WriteLine($"Is '{test}' a good binary string? => {IsGoodBinaryString(test)}");
+        }
+    }
+}
